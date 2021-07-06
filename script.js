b561768d285ff87c85f6322c964ca68a30729234
seacrest <cassiep1986@gmail.com>
2021-07-06 16:40:48 -0700

    saved form data to local storage

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-var today = moment();
+const currenttime = +moment().format("H");
+
+const today = moment();
 $("#currentDay").text(today.format("dddd, MMMM, Do, YYYY"));
 
+
+
+$(".container").each(function () { 
+    const time = $(this).attr("id").split("-").pop();
+ 
+    if (currenttime === time) {
+        $(this).addClass("present");
+    } else if (currenttime < time) {
+        $(this).addClass("future");
+    } else {
+        $(this).addClass("past");
+    };
+
+});
+window.onbeforeunload = function() {
+    localStorage.setItem(keep, $('.description').val());
+};
+
+window.onload = function() {
+
+    localStorage.getItem(".description").val;
+ 
+};
+
+$(".saveBtn").click (function () {
+    localStorage.keep = $('.description').val();
+
+});
 
 
